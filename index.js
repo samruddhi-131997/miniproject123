914abc532c93d9e666ec95b7085fdaec51037cbb
samruddhi-131997 <114893553+samruddhi-131997@users.noreply.github.com>
2022-11-08 07:40:57 +0530

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const hour1= document.getElementById("hour");
+const min1= document.getElementById("min");
+const sec1= document.getElementById("sec");
+const ampm1= document.getElementById("ampm");
+function updateClock(){
+    let h = new Date().getHours();
+    let m= new Date().getMinutes();
+    let s= new Date().getSeconds();
+    let ampm = "AM";
+
+    if(h>12){
+        h = h- 12;
+        ampm= "PM";
+        }
+    h = h<10 ? "0" + h : h;
+    s = s<10 ? "0" + s : s;
+    m = m<10 ? "0" + m : m;
+
+
+    hour1.innerText = h;
+    min1.innerText = m;
+    sec1.innerText = s;
+    ampm, (innerText = ampm);
+    setTimeout(()=>{
+    updateClock()
+    }, 1000)
+}
+
+updateClock();
